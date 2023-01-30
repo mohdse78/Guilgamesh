0a94e7f148c0639d61ceda2d288e09265d57e26e
Amirhosein <amirhoseinrad25@gmail.com>
2023-01-30 22:56:42 +0330

    feat: Improved aside and installed tailwind

--- my-project/src/app/pages/aside/aside.component.ts
+++ my-project/src/app/pages/aside/aside.component.ts
@@ -7,12 +7,18 @@ import {Component} from '@angular/core';
 })
 export class AsideComponent {
   items: any[] = [
-    {title: 'Feed', icon: 'feed.svg'},
-    {title: 'Just OKs', icon: 'check-circle.svg'},
-    {title: 'Review', icon: 'magic-star.svg'},
-    {title: 'Message', icon: 'email.svg'},
-    {title: 'Saved', icon: 'book-mark.svg'},
-    {title: 'Settings', icon: 'setting.svg'},
+    {id: 1, title: 'Feed', icon: 'feed.svg'},
+    {id: 2, title: 'Just OKs', icon: 'check-circle.svg'},
+    {id: 3, title: 'Review', icon: 'magic-star.svg'},
+    {id: 4, title: 'Message', icon: 'email.svg'},
+    {id: 5, title: 'Saved', icon: 'book-mark.svg'},
+    {id: 6, title: 'Settings', icon: 'setting.svg'},
   ]
 
+
+  selectedItem(id: number) {
+    this.items.forEach(item => item.isSelect = false);
+    let itemSelected = this.items.find(item => item.id == id);
+    itemSelected.isSelect = true;
+  }
 }
