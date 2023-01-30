89b29fc9ce95d3aab8a6713ac411f7827dbf8da0
Amirhosein <amirhoseinrad25@gmail.com>
2023-01-30 21:44:01 +0330

    chore: Improved structure

--- my-project/src/app/app-routing.module.ts
+++ my-project/src/app/app-routing.module.ts
@@ -1,22 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {FirstComponent} from "./first/first.component";
 import {SecondComponent} from "./second/second.component";
 import {ThirdComponent} from "./third/third.component";
 import {AppComponent} from "./app.component";
 
 const routes: Routes = [
-  {path:'', component: AppComponent},
-  {path:'first-validation',component: FirstComponent},
-  {path:'second-validation',component: SecondComponent},
-  {path:'third-validation',component: ThirdComponent}
+  {path: '', component: AppComponent},
+  {path: 'first-validation', component: FirstComponent},
+  {path: 'second-validation', component: SecondComponent},
+  {path: 'third-validation', component: ThirdComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
 
 export const ArrayOfComponents = [FirstComponent, SecondComponent, ThirdComponent];
 
