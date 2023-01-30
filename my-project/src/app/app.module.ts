302529e1476ba9423fc17d9d1db71fbf853bd7f4
mohaddeseh <mohadeseganjalinia@gmail.com>
2023-01-30 19:55:40 +0330

    Feat: project started

--- my-project/src/app/app.module.ts
+++ my-project/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule, ArrayOfComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArrayOfComponents
   ],
   imports: [
     BrowserModule,
