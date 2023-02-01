80e3be5b064f894e2c1337f7608e6803109f3514
mohaddeseh <mohadeseganjalinia@gmail.com>
2023-02-01 19:45:51 +0330

    docs: Completed third

--- my-project/src/app/third/third.component.ts
+++ my-project/src/app/third/third.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {StepService} from "../modules/core/services/step.service";
+import {stringify} from "postcss";
 
 @Component({
   selector: 'app-third',
@@ -7,11 +8,25 @@ import {StepService} from "../modules/core/services/step.service";
   styleUrls: ['./third.component.scss']
 })
 export class ThirdComponent {
+  cards: any = [
+    {id: 1, name: 'Statistical analysis and computing'},
+    {id: 2, name: 'Database management and architecture'},
+    {id: 3, name: 'Cryptocurrency'},
+    {id: 4, name: 'Business intelligence'}
+  ];
+  nameTag: any;
+  newcard: any;
 
   constructor(private stepService: StepService) {
   }
 
   ngOnInit(): void {
     this.stepService.setLevel(3);
+    if (localStorage.getItem('cards'))
+      this.cards = this.cards.concat(JSON.parse(<string>localStorage.getItem('cards')));
   }
+
+  // addingCard() {
+  //   localStorage.setItem('done', JSON.stringify(this.newcard));
+  // }
 }
