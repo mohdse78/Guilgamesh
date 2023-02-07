2d07a02162cc96c3859f45dfba3683dcf280fb6a
mohaddeseh <mohadeseganjalinia@gmail.com>
2023-02-07 21:31:29 +0330

    fix: trying to responsive

--- my-project/src/app/app.module.ts
+++ my-project/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatDatepickerModule} from "@angular/material/datepicker";
 import {MatNativeDateModule} from "@angular/material/core";
 import {MatLegacyChipsModule} from "@angular/material/legacy-chips";
 import {MatAutocompleteModule} from "@angular/material/autocomplete";
+import {MatSidenavModule} from "@angular/material/sidenav";
 
 
 @NgModule({
@@ -36,7 +37,8 @@ import {MatAutocompleteModule} from "@angular/material/autocomplete";
     MatDatepickerModule,
     MatNativeDateModule,
     MatAutocompleteModule,
-    MatLegacyChipsModule
+    MatSidenavModule,
+    MatLegacyChipsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
