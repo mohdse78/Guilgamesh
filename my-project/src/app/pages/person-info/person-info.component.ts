7d55383564b82b02229dfce3ce2ba6e3985c6ce7
mohaddeseh <mohadeseganjalinia@gmail.com>
2023-02-07 18:36:59 +0330

    docs: info component completed

--- my-project/src/app/pages/person-info/person-info.component.ts
+++ my-project/src/app/pages/person-info/person-info.component.ts
@@ -1,25 +1,30 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
 import {StepService} from "../../modules/core/services/step.service";
 import {FormBuilder} from "@angular/forms";
 import {CitiesService} from "../../modules/core/services/cities.service";
 import {Router} from "@angular/router";
+import {CommonsService} from "../../modules/core/services/commons.service";
 
 @Component({
   selector: 'app-person-info',
   templateUrl: './person-info.component.html',
   styleUrls: ['./person-info.component.scss']
 })
-export class PersonInfoComponent implements OnInit, OnDestroy {
-  cards: any = [
-    {id: 1, name: 'Statistical analysis and computing'},
-    {id: 2, name: 'Database management and architecture'},
-    {id: 3, name: 'Cryptocurrency'},
-    {id: 4, name: 'Business intelligence'}
-  ];
+export class PersonInfoComponent implements OnInit, AfterViewInit, OnDestroy {
+  loaded: boolean = false;
   myForm: any;
+  type: any;
+  paymentType: any;
+  timeframe: any;
+  availability: any;
+  location: any;
+  university: any;
+  tagline: any = [];
 
 
   constructor(private stepService: StepService,
+              private commonService: CommonsService,
+              private citiesService: CitiesService,
               private router: Router
   ) {
     this.myForm = this.router.getCurrentNavigation()?.extras.state;
@@ -27,7 +32,95 @@ export class PersonInfoComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.stepService.setStatus(false);
-    console.log(this.myForm.value)
+    this.getUniversity();
+    this.getLocation();
+    this.getType();
+    this.getPaymentType();
+    this.getTagline();
+    this.getAvailibility();
+    this.getTimeFram();
+  }
+
+  getUniversity() {
+    this.commonService.getUniversity().subscribe((res: any) => {
+      let allUniversities = res;
+      allUniversities.map((x: any) => {
+        if (this.myForm.data.universityId == x.id) {
+          this.university = x.title;
+        }
+      })
+    })
+  }
+
+  getPaymentType() {
+    if (this.myForm.data.paymentType == 0) {
+      this.paymentType = "per hour";
+    } else if (this.myForm.data.paymentType == 1) {
+      this.paymentType = "per day";
+    } else if (this.myForm.data.paymentType == 2) {
+      this.paymentType = "per week";
+    } else {
+      this.paymentType = "per month";
+    }
+  }
+
+  getLocation() {
+    this.citiesService.getCities().subscribe((res: any) => {
+      let allCities = res.model;
+      allCities.map((x: any) => {
+        if (x.key == this.myForm.data.cityId) {
+          this.location = x.value;
+        }
+      })
+    })
+  }
+
+  getType() {
+    if (this.myForm.data.contractType == 0) {
+      this.type = "In Person";
+    } else if (this.myForm.data.contractType == 1) {
+      this.type = "Remote";
+    } else {
+      this.type = "Hybrid";
+    }
+  }
+
+  getTimeFram() {
+    let difTimeFram = Math.round(this.myForm.data.validTo.getTime() - this.myForm.data.validFrom.getTime());
+    let diffCount = difTimeFram / (1000 * 3600 * 24);
+    if (diffCount < 30) {
+      this.timeframe = diffCount + " Days";
+    } else {
+      let monthCounter = Math.round(diffCount / 30);
+      this.timeframe = monthCounter + " Months"
+    }
+  }
+
+  getAvailibility() {
+    if (this.myForm.data.availability == 0) {
+      this.availability = "Full Time";
+    } else if (this.myForm.data.availability == 1) {
+      this.availability = "Part Time";
+    } else {
+      this.availability = "Seasonal Say";
+    }
+  }
+
+  getTagline() {
+    this.commonService.getJobTitles().subscribe((res: any) => {
+      let allTags = res;
+      this.myForm.data.tagline.map((x: any) => {
+        allTags.map((y: any) => {
+          if (y.id == x.jobTitleId) {
+            this.tagline.push(y.title);
+          }
+        })
+      })
+    })
+  }
+
+  ngAfterViewInit() {
+    this.loaded = true;
   }
 
   ngOnDestroy(): void {
